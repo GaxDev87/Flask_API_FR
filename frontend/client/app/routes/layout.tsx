0cc6464806e7c85bdc8646240aa5e0879b2f4c36
Zahoris.Leonardo <zahoris.leonardo@one-beyond.com>
2024-01-04 11:13:54 +0100

    adding code

--- frontend/client/app/routes/layout.tsx
+++ frontend/client/app/routes/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen w-full border-r-indigo-600 font-mono">
+    <div className="h-screen w-full border-r-lime-500 font-mono">
       {children}
     </div>
   );
